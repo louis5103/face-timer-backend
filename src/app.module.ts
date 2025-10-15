ffc89d16950d85b236360ac21b361e26ee8f9ddd
louis5103 <louis38625103@gmail.com>
2025-10-16 00:08:19 +0900

    [Refactor] app.module 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TimerModule } from './timer/timer.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { RankingModule } from './ranking/ranking.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    AuthModule,
+    TasksModule,
+    TimerModule,
+    StatisticsModule,
+    RankingModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
